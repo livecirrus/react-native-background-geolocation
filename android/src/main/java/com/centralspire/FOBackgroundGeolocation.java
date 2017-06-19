defbab4b4578626a4e7a24c8ab096dca42dd68ee
Lida Tang <lida@livecirrus.com>
2017-06-19 01:52:11 -0400

    extract config from responseText

--- android/src/main/java/com/centralspire/FOBackgroundGeolocation.java
+++ android/src/main/java/com/centralspire/FOBackgroundGeolocation.java
@@ -7,34 +7,37 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class FOBackgroundGeolocation {
-  static public void configureGeolocation(BackgroundGeolocation adapter, JSONObject config) {
-    TSCallback callback = new TSCallback() {
-      @Override
-      public void success(Object o) {
+  static public void configureGeolocation(BackgroundGeolocation adapter, JSONObject response) {
+    if (response.has("responseText")) {
+      JSONObject config = response.getJSONObject("responseText");
+      TSCallback callback = new TSCallback() {
+        @Override
+        public void success(Object o) {
+        }
+        @Override
+        public void error(Object o) {
+        }
+      }; // empty callback
+      try {
+        if (config.has("config")) {
+          adapter.setConfig(config.getJSONObject("config"), callback);
+        }
+        if (config.has("stop")) {
+          adapter.stop(callback);
+        }
+        if (config.has("start")) {
+          adapter.start(callback);
+        }
+        if (config.has("geofences")) {
+          adapter.removeGeofences(new JSONArray(), callback);
+          adapter.addGeofences(config.getJSONArray("geofences"), callback);
+        }
+        if (config.has("startGeofences")) {
+          adapter.stop(callback);
+          adapter.startGeofences(callback);
+        }
+      } catch (JSONException e) {
       }
-      @Override
-      public void error(Object o) {
-      }
-    }; // empty callback
-    try {
-      if (config.has("config")) {
-        adapter.setConfig(config.getJSONObject("config"), callback);
-      }
-      if (config.has("stop")) {
-        adapter.stop(callback);
-      }
-      if (config.has("start")) {
-        adapter.start(callback);
-      }
-      if (config.has("geofences")) {
-        adapter.removeGeofences(new JSONArray(), callback);
-        adapter.addGeofences(config.getJSONArray("geofences"), callback);
-      }
-      if (config.has("startGeofences")) {
-        adapter.stop(callback);
-        adapter.startGeofences(callback);
-      }
-    } catch (JSONException e) {
     }
   }
 }
