c53e0c8a47a3518150b033f2214e0da740a4b555
Lida Tang <lida@livecirrus.com>
2017-06-18 23:15:13 -0400

    handle json exception and instantiate abstract tscallback

--- android/src/main/java/com/centralspire/FOBackgroundGeolocation.java
+++ android/src/main/java/com/centralspire/FOBackgroundGeolocation.java
@@ -4,26 +4,37 @@ import com.transistorsoft.locationmanager.adapter.BackgroundGeolocation;
 import com.transistorsoft.locationmanager.adapter.TSCallback;
 import org.json.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONException;
 
 public class FOBackgroundGeolocation {
   static public void configureGeolocation(BackgroundGeolocation adapter, JSONObject config) {
-    TSCallback callback = new TSCallback(); // empty callback
-    if (config.has("config")) {
-      adapter.setConfig(config.getJSONObject("config"), callback);
-    }
-    if (config.has("stop")) {
-      adapter.stop(callback);
-    }
-    if (config.has("start")) {
-      adapter.start(callback);
-    }
-    if (config.has("geofences")) {
-      adapter.removeGeofences(new JSONArray(), callback);
-      adapter.addGeofences(config.getJSONArray("geofences"), callback);
-    }
-    if (config.has("startGeofences")) {
-      adapter.stop(callback);
-      adapter.startGeofences(callback);
+    TSCallback callback = new TSCallback() {
+      @Override
+      public void success(Object o) {
+      }
+      @Override
+      public void error(Object o) {
+      }
+    }; // empty callback
+    try {
+      if (config.has("config")) {
+        adapter.setConfig(config.getJSONObject("config"), callback);
+      }
+      if (config.has("stop")) {
+        adapter.stop(callback);
+      }
+      if (config.has("start")) {
+        adapter.start(callback);
+      }
+      if (config.has("geofences")) {
+        adapter.removeGeofences(new JSONArray(), callback);
+        adapter.addGeofences(config.getJSONArray("geofences"), callback);
+      }
+      if (config.has("startGeofences")) {
+        adapter.stop(callback);
+        adapter.startGeofences(callback);
+      }
+    } catch (JSONException e) {
     }
   }
 }
